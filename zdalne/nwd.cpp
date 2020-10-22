b89b774ca30ef51684dc233ed39e85255d3327e3
Filipasek <filipasek10@gmail.com>
2020-10-22 11:00:41 +0200

    nwd updated

--- zdalne/nwd.cpp
+++ zdalne/nwd.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    int a, b, x, y;
-    cout<<"Podaj liczby: "<<endl;
-    cin>>a>>b;
-    x = a; //placeholder
-    y = b; //placeholder asweel
+int obliczNWD(int a, int b){
     while(a != b){
         if(a > b) a = a - b;
         else if(b > a) b = b - a;
     }
+    return a;
+}
+
+int main(){
+    int x, y;
+    cout<<"Podaj liczby: "<<endl;
+    cin>>x>>y;
+    int a = obliczNWD(x, y);
     cout<<endl<<"NWD liczb "<<x<<" i "<<y<<" jest rowny: "<<a;
     return 0;
 }
